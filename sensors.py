5b170ec212b3710ad3159c79f07b2701d09ee1b2
Raul <raul.duran@alumnos.upm.es>
2018-05-22 20:50:29 +0200

    cambios

--- sensors.py
+++ sensors.py
@@ -66,11 +66,4 @@ class Sensor:
         distance = distance / 100
         if distance > 1.0:
             distance = 1.0
-
-<<<<<<< HEAD
-        #print "Sensor: ", self.TRIGGER, " ", self.ECHO, " PULSE -> ", distance
-=======
- #       print "Sensor: ", self.TRIGGER, " ", self.ECHO, " PULSE -> ", distance
->>>>>>> dfea59b5f76068fa310aeea9594b91a1a77605a0
-
         return distance
